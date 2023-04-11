92e6b0ad07d5700586a69bec0b4ad3620fc0ff1c
Zohrab Taghiyev <55695927+z0hrab@users.noreply.github.com>
2023-04-11 10:41:51 +0400

    Initial Setup
    Nothing special.

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by zed on 11.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+

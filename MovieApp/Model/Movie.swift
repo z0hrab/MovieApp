0d92fa432d131721bad2adc667365adb58b87d70
Zohrab Taghiyev <55695927+z0hrab@users.noreply.github.com>
2023-04-18 19:40:59 +0400

    Revision

--- MovieApp/Model/Movie.swift
+++ MovieApp/Model/Movie.swift
@@ -24,7 +24,7 @@ struct Result: Codable {
     let backdropPath: String
     let genreIDS: [Int]
     let id: Int
-    let originalLanguage: OriginalLanguage
+    let originalLanguage: String
     let originalTitle, overview: String
     let popularity: Double
     let posterPath, releaseDate, title: String
@@ -48,8 +48,8 @@ struct Result: Codable {
     }
 }
 
-enum OriginalLanguage: String, Codable {
-    case en = "en"
-    case es = "es"
-    case ko = "ko"
-}
+//enum OriginalLanguage: String, Codable {
+//    case en = "en"
+//    case es = "es"
+//    case ko = "ko"
+//}
